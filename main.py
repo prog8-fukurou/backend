fb8e28ca94e3a044a8c3c11b7db6727319627571
ten10ba <59526400+ten10ba@users.noreply.github.com>
2024-06-20 22:43:04 +0900

    画像を保存できるように設定

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import base64
 import json
 import boto3
+import os
 
 app = FastAPI()
 
@@ -93,17 +94,25 @@ async def generate_image():
         },
     }
 
-    # Convert the native request to JSON.
     request = json.dumps(native_request)
 
-    # Invoke the model with the request.
     response = client.invoke_model(modelId=model_id, body=request)
 
-    # Decode the response body.
     model_response = json.loads(response["body"].read())
 
-    # Extract the image data.
     base64_image_data = model_response["images"][0]
+    
+    # i, output_dir = 1, "output"
+    # if not os.path.exists(output_dir):
+    #     os.makedirs(output_dir)
+    # while os.path.exists(os.path.join(output_dir, f"titan_{i}.png")):
+    #     i += 1
+
+    # image_data = base64.b64decode(base64_image_data)
+
+    # image_path = os.path.join(output_dir, f"titan_{i}.png")
+    # with open(image_path, "wb") as file:
+    #     file.write(image_data)
         
     return {"image": base64_image_data}
 
