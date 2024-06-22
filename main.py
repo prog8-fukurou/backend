7536ca807fbb8695cd70b331de3cd1e206959555
sasanqua <78304794+sasanqua-dev@users.noreply.github.com>
2024-06-22 15:22:56 +0900

    feat: Add logic to start game when all players are ready

--- main.py
+++ main.py
@@ -104,6 +104,14 @@ async def websocket_endpoint(websocket: WebSocket, client_id: str, room_id: int 
                     await room.broadcast_message(f"{client_id}:{msg.split(':')[1]}")
                 case "user-ready":
                     room.readied.add(client_id)
+                    if ":" in msg:
+                        if msg.split(":")[1] == "debug:master":
+                            if len(room.readied) == 2:
+                                room.master = client_id
+                                await room.broadcast_message(f"game-start:{room.master}")
+                        else:
+                            if len(room.readied) == 2:
+                                await room.broadcast_message(f"game-start:{room.master}")
                     if len(room.readied) == 4:
                         master_index = random.randint(0, len(room.players) - 1)
                         room.master = list(room.players)[master_index]
