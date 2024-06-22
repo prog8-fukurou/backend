ef2c5bc74e0f5136b7d22d3e95d771e1a9b04219
ten10ba <59526400+ten10ba@users.noreply.github.com>
2024-06-22 17:20:12 +0900

    文章と画像生成

--- main.py
+++ main.py
@@ -131,14 +131,27 @@ class PromptMaterial(BaseModel):
     background_color: str | None
     belongings: str | None
 
+class ResponseMaterial(BaseModel):
+    travel_plan_name: str | None
+    travel_place: str | None
+    travel_schedule: list[str] | None
+    suggested_sightseeing_spots: list[str] | None
+    travel_plan_description: str | None
+    belongings: str | None
+    
 @app.post("/prompt")
-async def generate_text(prompt: PromptMaterial):
+async def generate_text(prompt: PromptMaterial, try_count: int = 0):
     if prompt.purpose is None and prompt.category is None and prompt.overnight is None and prompt.belongings is None:
         raise HTTPException(status_code=422, detail="Please specify at least one parameter.")
-    client = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
-    model_id = "anthropic.claude-3-haiku-20240307-v1:0"
+    client = boto3.client(service_name="bedrock-runtime", region_name = os.environ['AWS_DEFAULT_REGION'])
+    model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
     # Start a conversation with the user message.
-    user_message = "この旅行の持ち物を教えてください"
+    user_message = f"Human:\nあなたは優秀なアシスタントです。\n以下に旅行の資料を添付します。\n\n"
+    user_message += f"<document>\n旅の目的:{prompt.purpose}\nどんな場所か:{prompt.category}\n日帰りか泊まりか:{(lambda x: '日帰り' if prompt.overnight == '日帰り' else '3泊4日')}\n持ち物:{prompt.belongings}</document>\n\n"
+    user_message += f"以上の資料をもとに、旅行のプラン名、目的地の名前、持ち物、旅行スケジュール、おすすめの観光スポット、プランの説明を考え、'travel_plan_name','travel_place','travel_schedule','suggested_sightseeing_spots','travel_plan_description','belongings'をキーとしたJSON形式で生成してください。\n\n"
+    user_message += f"ただし、旅行スケジュールとおすすめの観光スポットは文字列のリスト形式にしてください。目的地や観光スポットは固有名詞にしてください。また、目的地や観光スポットは実在するものでも、架空のものでも、構いません。JSONデータ以外は生成しないでください。"
+    user_message += f"Assistant:\n"
+    
     conversation = [
         {
             "role": "user",
@@ -151,12 +164,22 @@ async def generate_text(prompt: PromptMaterial):
         response = client.converse(
             modelId=model_id,
             messages=conversation,
-            inferenceConfig={"maxTokens": 300, "temperature": 0.5, "topP": 0.9},
+            inferenceConfig={"maxTokens": 2000, "temperature": 0.5, "topP": 0.9},
         )
 
         # Extract and print the response text.
         response_text = response["output"]["message"]["content"][0]["text"]
         print(response_text)
+        try:
+            json_response = json.loads(response_text)
+            prompt_material_response = ResponseMaterial(**json_response)
+        except (json.JSONDecodeError, KeyError) as e:
+            print(f"Error parsing response: {e}")
+            if try_count < 3:
+                return await generate_text(prompt, try_count + 1)
+            else:
+                raise HTTPException(status_code=500, detail="Failed to generate valid text after multiple attempts.")
+        return response_text
 
     except (ClientError, Exception) as e:
         print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
@@ -164,26 +187,37 @@ async def generate_text(prompt: PromptMaterial):
 
     # StreamResponseにしたい
     # https://engineers.safie.link/entry/2022/11/14/fastapi-streaming-response
-    text = aws_generate_text(prompt)
-    return text
 
 @app.post("/image")
-async def generate_image(prompt: str):
+async def generate_image(prompt: ResponseMaterial):
+    prompt_material_response = prompt
+    if prompt_material_response.travel_plan_name is None and prompt_material_response.travel_place is None and prompt_material_response.travel_schedule is None and prompt_material_response.suggested_sightseeing_spots is None and prompt_material_response.travel_plan_description is None and prompt_material_response.belongings is None:
+        raise HTTPException(status_code=422, detail="Please specify at least one parameter.")
     client = boto3.client(service_name="bedrock-runtime", region_name = os.environ['AWS_DEFAULT_REGION'])
-    model_id = "amazon.titan-image-generator-v1"
+    # model_id = "amazon.titan-image-generator-v1"
+    model_id = "stability.stable-diffusion-xl-v1"
+    user_message = f"{prompt_material_response.travel_plan_name}という旅行で、{prompt_material_response.travel_place}という観光地を訪れます。ただし、{prompt_material_response.travel_place}には{','.join(prompt_material_response.suggested_sightseeing_spots)}という観光地があります。\n\n以上の情報を踏まえて、{prompt_material_response.travel_place}のイメージ画像を生成してください。"
     seed = random.randint(0, 2147483647)
+    print(f"user_message: {user_message}")
     # Format the request payload using the model's native structure.
+    # native_request = {
+    #     "taskType": "TEXT_IMAGE",
+    #     "textToImageParams": {"text": user_message},
+    #     "imageGenerationConfig": {
+    #         "numberOfImages": 1,
+    #         "quality": "standard",
+    #         "cfgScale": 8.0,
+    #         "height": 512,
+    #         "width": 512,
+    #         "seed": seed,
+    #     },
+    # }
     native_request = {
-        "taskType": "TEXT_IMAGE",
-        "textToImageParams": {"text": prompt},
-        "imageGenerationConfig": {
-            "numberOfImages": 1,
-            "quality": "standard",
-            "cfgScale": 8.0,
-            "height": 512,
-            "width": 512,
-            "seed": seed,
-        },
+        "text_prompts": [{"text": user_message}],
+        "style_preset": "photographic",
+        "seed": seed,
+        "cfg_scale": 10,
+        "steps": 30,
     }
 
     request = json.dumps(native_request)
@@ -192,19 +226,25 @@ async def generate_image(prompt: str):
 
     model_response = json.loads(response["body"].read())
 
-    base64_image_data = model_response["images"][0]
+    # base64_image_data = model_response["images"][0]
+    base64_image_data = model_response["artifacts"][0]["base64"]
+
     
-    # i, output_dir = 1, "output"
-    # if not os.path.exists(output_dir):
-    #     os.makedirs(output_dir)
+    i, output_dir = 1, "output"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     # while os.path.exists(os.path.join(output_dir, f"titan_{i}.png")):
     #     i += 1
+    while os.path.exists(os.path.join(output_dir, f"stablediffusion_{i}.png")):
+        i += 1
 
-    # image_data = base64.b64decode(base64_image_data)
+    image_data = base64.b64decode(base64_image_data)
 
     # image_path = os.path.join(output_dir, f"titan_{i}.png")
-    # with open(image_path, "wb") as file:
-    #     file.write(image_data)
+    image_path = os.path.join(output_dir, f"stablediffusion_{i}.png")
+    
+    with open(image_path, "wb") as file:
+        file.write(image_data)
         
     return {"image": base64_image_data}
 
